af160558fd3ebf0505f622b50e14c7292af44caa
Julien <julienmichelin35@gmail.com>
2025-03-03 20:19:14 +0100

    Ajout d'une ToDo list, changement sur index.js, ajout de l'envoie de la note vers le serveur et création d'une fonction permettant l'affichage des notes

--- projet_moyenne/static/index.js
+++ projet_moyenne/static/index.js
@@ -2,30 +2,35 @@ const envoyerNote = document.getElementById("note");
 const nouvelleNote = document.getElementById("nouvelle-note");
 const noteSur = document.getElementById("note-sur");
 const divNote = document.getElementById("add-note");
-//écoute la réponse du formulaire d'envoie des messages
-envoyerNote.addEventListener('submit', function(event){
-    //empèche le comportement par défaut de l'évènement: rafraichir la page
-    event.preventDefault();
-    var note = nouvelleNote.value;
-    var sur = noteSur.value;
-    
+
+// Initialise la connexion socketIO
+const socket = io();
+
+/**
+ * Ajoute une note a la page HTML
+ * @param {note obtenue} note 
+ * @param {Sur combien est la note} sur 
+ */
+function addNote(note, sur){
     const addNote = document.createElement('p');
     addNote.textContent = note + '/' + sur;
 
     divNote.append(addNote)
+}
 
+//écoute la réponse du formulaire d'envoie des messages
+envoyerNote.addEventListener('submit', function(event){
+
+    //empèche le comportement par défaut de l'évènement: rafraichir la page
+    event.preventDefault();
+
+    //Récupère la valeur de la note et sur combien elle est
+    const note = [nouvelleNote.value, noteSur.value];
+
+    // Envoie de la note sur le serveur
+    socket.emit('nouvelle-note', note);
 
     //vide le formulaire
     nouvelleNote.value = '';
     noteSur.value = '';
-});
-function runPy() {
-    $.ajax({
-        type:'POST',
-        url:'test.py',
-        success: function(data) {                                                     
-            console.log(data);
-        }
-    });
-}
-runPy()
+})
